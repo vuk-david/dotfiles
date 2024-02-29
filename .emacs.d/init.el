e4f097e090899bbf2afd90b0bea7c0ab5e277c57
vuk-david <116966837+vuk-david@users.noreply.github.com>
2024-02-29 22:12:49 +0100

    Add hook for a startup time-measurement

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -2,6 +2,16 @@
 ;; ========================== EMACS CONFIGURATION FILE ====================
 ;; ========================================================================
 
+;; Use a hook so the message doesn't get clobbered by other messages.
+(add-hook 'emacs-startup-hook
+          (lambda ()
+            (message "Emacs ready in %s with %d garbage collections."
+                     (format "%.2f seconds"
+                             (float-time
+                              (time-subtract after-init-time before-init-time)))
+                     gcs-done)))
+
+
 ;;(setq mode-line-format nil)
 
 ;; ---------------------------------------------------------------------
